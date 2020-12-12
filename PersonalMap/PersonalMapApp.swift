ba200cf035f5142e107baa9c80b26550d0f93aa9
Kato Hidetoshi <maildhl@digital-hub-app.work>
2020-12-12 16:02:17 +0900

    Initial Commit

---
+++ PersonalMap/PersonalMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalMapApp.swift
+//  PersonalMap
+//
+//  Created by Digital Hub Life on 2020/12/12.
+//
+
+import SwiftUI
+
+@main
+struct PersonalMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
