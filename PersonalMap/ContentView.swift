752a4d6b35455cdd7cbb67f5272b1611704e2d76
Kato Hidetoshi <maildhl@digital-hub-app.work>
2020-12-20 16:42:51 +0900

    初期画面設定

--- PersonalMap/ContentView.swift
+++ PersonalMap/ContentView.swift
@@ -1,16 +1,70 @@
-//
-//  ContentView.swift
-//  PersonalMap
-//
-//  Created by Digital Hub Life on 2020/12/12.
-//
-
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State var locations: [CLLocationCoordinate2D] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .topLeading){
+            MapView(locations: $locations) { location in
+                print(location)
+            }
+            HStack{
+                Button(action: {}) {
+                    Image(systemName: "plus.rectangle.on.rectangle")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.blue, lineWidth: 2))
+                }
+                Button(action: {}) {
+                    Image(systemName: "plus.rectangle.on.rectangle")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.blue, lineWidth: 2))
+                }
+                Button(action: {}) {
+                    Image(systemName: "plus.rectangle.on.rectangle")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.blue, lineWidth: 2))
+                }
+                Button(action: {}) {
+                    Image(systemName: "plus.rectangle.on.rectangle")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.blue, lineWidth: 2))
+                }
+                Button(action: {}) {
+                    Image(systemName: "plus.rectangle.on.rectangle")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.blue, lineWidth: 2))
+                }
+            }
+            .padding(.top, 48)
+            .padding(.leading,32)
+
+        }
+        
+        
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .ignoresSafeArea(.all, edges: .all)
     }
 }
 
