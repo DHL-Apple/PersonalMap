f670c9317c554f109b4033656e275e6b8d72a5cf
Kato Hidetoshi <maildhl@digital-hub-app.work>
2020-12-21 07:13:51 +0900

    アクションシーとのコードを追加。 表示のさせかたが良くわかりませんでした。

--- PersonalMap/ContentView.swift
+++ PersonalMap/ContentView.swift
@@ -17,49 +17,67 @@ struct ContentView: View {
                         .background(Color("Button-blue"))
                         .cornerRadius(5)
                         .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.blue, lineWidth: 2))
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
                 }
                 Button(action: {}) {
-                    Image(systemName: "plus.rectangle.on.rectangle")
+                    Image(systemName: "square.stack.3d.up")
                         .padding(8)
                         .background(Color("Button-blue"))
                         .cornerRadius(5)
                         .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.blue, lineWidth: 2))
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
                 }
                 Button(action: {}) {
-                    Image(systemName: "plus.rectangle.on.rectangle")
+                    Image(systemName: "car")
                         .padding(8)
                         .background(Color("Button-blue"))
                         .cornerRadius(5)
                         .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.blue, lineWidth: 2))
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
                 }
                 Button(action: {}) {
-                    Image(systemName: "plus.rectangle.on.rectangle")
+                    Image(systemName: "airplane")
                         .padding(8)
                         .background(Color("Button-blue"))
                         .cornerRadius(5)
                         .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.blue, lineWidth: 2))
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
                 }
                 Button(action: {}) {
-                    Image(systemName: "plus.rectangle.on.rectangle")
+                    Image(systemName: "lightbulb")
                         .padding(8)
                         .background(Color("Button-blue"))
                         .cornerRadius(5)
                         .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.blue, lineWidth: 2))
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
+                }
+                Button(action: {}) {
+                    Image(systemName: "lightbulb.fill")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
+                }
+                Button(action: {}) {
+                    Image(systemName: "gearshape")
+                        .padding(8)
+                        .background(Color("Button-blue"))
+                        .cornerRadius(5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.blue, lineWidth: 2))
                 }
             }
             .padding(.top, 48)
             .padding(.leading,32)
-
+            
         }
         
         
@@ -73,3 +91,36 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//struct ActionSheetSample: View {
+//    @State private var isShowActionSheet: Bool =
+//        false
+//    var body: some View {
+//        Button(action: {
+//            //アクションシートを表示
+//            isShowActionSheet = true
+//        }) {
+//            Image(systemName: "car")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 60, height: 60)
+//        }
+//        .actionSheet(isPresented: $isShowActionSheet)
+//        { () -> ActionSheet in ActionSheet(title: Text("新規データ登録"),message: Text("地図に登録するデータの種類を選択してください。"),
+//                                           buttons: [
+//                                            .default(Text("ポイント"),action: {
+//                                                print("ポイントを選んだ")
+//                                            }),
+//                                            .default(Text("ライン"),action: {
+//                                                print("ラインを選んだ")
+//                                            }),.default(Text("エリア"),action: {
+//                                                print("エリアを選んだ")
+//                                            }),
+//                                            .cancel(Text("選択しない"),action: {
+//                                                print("選択しない")
+//                                            })
+//                                           ])
+//        }
+//    }
+//}
